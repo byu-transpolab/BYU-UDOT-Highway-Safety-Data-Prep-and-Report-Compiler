997466c44161e96f417cb0de8ce934a1a859b680
Tomas Barriga <barriga.tommy@gmail.com>
2021-10-25 15:41:02 -0600

    Prepare to add roadway data to RGUI
    Create new scripts for crash and roadway data prep

---
+++ R/RGUI_RoadwayPrep.R
@@ -0,0 +1,43 @@
+# The following is the functions that prepare the roadway data to be joined
+
+# Keeps the following columns from the aadt file
+check_aadt <- function(table){
+  table %>%
+    select()
+}
+
+# Keeps the following columns from the functional class file
+check_functionalclass <- function(table){
+  table %>%
+    select()
+}
+
+# Keeps the following columns from the intersections file
+check_intersections <- function(table){
+  table %>%
+    select()
+}
+
+# Keeps the following columns from the lanes file
+check_lanes <- function(table){
+  table %>%
+    select()
+}
+
+# Keeps the following columns from the pavement messages file
+check_pavementmessages <- function(table){
+  table %>%
+    select()
+}
+
+# Keeps the following columns from the speed limits file
+check_speedlimits <- function(table){
+  table %>%
+    select()
+}
+
+# Keeps the following columns from the urban code file
+check_urbancode <- function(table){
+  table %>%
+    select()
+}

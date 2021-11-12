4bbde3a74b125aa35330942c2e1196614bbb739d
samlrun <85369403+samlrun@users.noreply.github.com>
2021-11-11 17:40:03 -0700

    Wrote code to find intersections near schools and UTA stops

--- R/runyan_code.R
+++ R/runyan_code.R
@@ -8,10 +8,10 @@ if (!require("pacman")) install.packages("pacman")
 
 # pacman must already be installed; then load contributed
 # packages (including pacman) with pacman
-pacman::p_load(magrittr, pacman, tidyverse)
+pacman::p_load(magrittr, pacman, tidyverse, readxl, sf)
 
 
-# FILTER CAMS CRASH DATA
+# FILTER CAMS CRASH DATA ###################################
 
 # change route 089A to 0011... There seems to be more to this though
 crash$ROUTE_ID[crash$ROUTE_ID == '089A'] <- 0011
@@ -24,3 +24,66 @@ crash %>%
   as_tibble() %>%
   filter(RAMP_ID == 0 & is.na(RAMP_ID))
   
+
+
+
+# Combine Additional Variables to Final Output #############
+
+# SPATIAL ANALYSIS
+
+rm("joinprep","location","vehicle","rollups")
+
+df <- crash %>%
+  select(crash_id,lat,long,crash_severity_id,weather_condition_id,manner_collision_id,pedalcycle_involved) %>%
+  filter(lat > 30 & long < -95 & !is.na(lat) & !is.na(long))
+
+crash_sf <- st_as_sf(df, 
+               coords = c("long", "lat"), 
+               crs = 4326,
+               remove = F)
+
+crash_sf <- st_transform(crash_sf, crs = 26912) #convert to UTM zone 12
+
+intersections <- read_sf("data/Intersections/Intersections.shp") %>%
+  st_transform(crs = 26912)
+
+UTA_stops <- read_sf("data/UTA_Stops/UTA_Stops_and_Most_Recent_Ridership.shp") %>%
+  st_transform(crs = 26912) %>%
+  select(UTA_StopID) %>%
+  st_buffer(dist = 304.8) #buffer 1000 ft (units converted to meters)
+
+schools <- read_sf("data/Utah_Schools_PreK_to_12/Utah_Schools_PreK_to_12.shp") %>%
+  st_transform(crs = 26912) %>%
+  select(SchoolID) %>%
+  st_buffer(dist = 304.8) #buffer 1000 ft (units converted to meters)
+
+# VISUALIZE
+
+# determine which intersections are 1000 ft from a school
+ints_near_schools <- st_join(intersections, schools, join = st_within) %>%
+  filter(!is.na(SchoolID))
+
+# detemine which intersections are 1000 ft from a UTA stop
+ints_near_UTA <- st_join(intersections, UTA_stops, join = st_within) %>%
+  filter(!is.na(UTA_StopID))
+
+plot(ints_near_schools["ID"])
+plot(ints_near_UTA["ID"])
+plot(intersections["ID"])
+
+# COMBINE
+
+# modify intersections object
+intersections <- st_join(intersections, schools, join = st_within)
+intersections <- st_join(intersections, UTA_stops, join = st_within) %>%
+  st_drop_geometry()
+
+# remove old dfs
+rm("crash_sf","df","schools","UTA_stops","ints_near_schools","ints_near_UTA")
+
+# additional intersection data
+#ints_extra <- read_csv("data/Intersection_w_ID_MEV_MP.csv")
+#intersections <- full_join(intersections, ints_extra)
+ 
+# save csv file
+write_csv(intersections, file = "data/Intersections_Compiled.csv")

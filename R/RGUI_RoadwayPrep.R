74b637cd52426158bc26978bb13d1430d583af71
samlrun <85369403+samlrun@users.noreply.github.com>
2021-11-19 15:33:20 -0700

    worked on headers

--- R/RGUI_RoadwayPrep.R
+++ R/RGUI_RoadwayPrep.R
@@ -1,41 +1,71 @@
 # The following is the functions that prepare the roadway data to be joined
 
 # Keeps the following columns from the aadt file
-check_aadt <- function(table){
-  table %>%
-    select(rt_num, start_accu,end_accum, station, aadrt2020 ,aadt2019, 
-           aadt2018, aadt2017, aadt2016,sutrk2020, cutrk2020,sutrk2019, 
-           cutrk2019,sutrk2018, cutrk2018,sutrk2017, cutrk2017,sutrk2016, 
-           cutrk2016,rt_type)
+check_aadt <- function(df){
+  df %>%
+    select(rt_num, 
+           start_accu,
+           end_accum, 
+           station, 
+           starts_with('aadt'),
+           #aadt2020, aadt2019, aadt2018, aadt2017, aadt2016,
+           starts_with('sutrk'),
+           starts_with('cutrk'),
+           #sutrk2020, cutrk2020, sutrk2019, cutrk2019, sutrk2018, cutrk2018, sutrk2017, cutrk2017, sutrk2016, cutrk2016,
+           rt_type)
 }
 
 # Keeps the following columns from the functional class file
-check_functionalclass <- function(table){
-  table %>%
-    select(from_measure, to_measure, functional_class, )
+check_functionalclass <- function(df){
+  df %>%
+    select(from_measure, 
+           to_measure, 
+           functional_class, 
+           )
 }
 
 # Keeps the following columns from the intersections file
-check_intersections <- function(table){
-  table %>%
-    select()
+check_intersections <- function(df){
+  df %>%
+    select(orig_int_id = ID,
+           lat = BEG_LAT,
+           long = BEG_LONG,
+           elevation = BEG_ELEV,
+           traffic_ctrl = TRAFFIC_CO,
+           sr_sr = SR_SR,
+           route = ROUTE,
+           int_rt_1 = INT_RT_1,
+           int_rt_2 = INT_RT_2,
+           int_rt_3 = INT_RT_3,
+           int_rt_4 = INT_RT_4,
+           udot_bmp = MANDLI_BMP,
+           int_type = INT_TYPE,
+           station = STATION,
+           region = REGION,
+           #group_num = GROUP_NUM  -can't find this one
+           num_schools = NUM_SCHOOLS,
+           num_uta = NUM_UTA)
 }
 
 # Keeps the following columns from the lanes file
-check_lanes <- function(table){
-  table %>%
-    select()
+check_lanes <- function(df){
+  df %>%
+    select(route = ROUTE,
+           beg_milepoint = START_ACCUM,
+           end_milepoint = END_ACCUM,
+           
+           )
 }
 
 # Keeps the following columns from the pavement messages file
-check_pavementmessages <- function(table){
-  table %>%
+check_pavementmessages <- function(df){
+  df %>%
     select()
 }
 
 # Keeps the following columns from the speed limits file
-check_speedlimits <- function(table){
-  table %>%
+check_speedlimits <- function(df){
+  df %>%
     select()
 }
 

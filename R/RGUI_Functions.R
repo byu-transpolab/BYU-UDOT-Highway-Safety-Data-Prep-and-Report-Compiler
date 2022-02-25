9e0ab76a5581470809ffb88b8637760e5892c6c9
samlrun <85369403+samlrun@users.noreply.github.com>
2022-02-25 10:40:15 -0700

    started segmentizing

--- R/RGUI_Functions.R
+++
@@ -1,8 +0,0 @@
-# Add spatial data to CAMS. Temporary solution to Excel GUI problem.
-add_spatial <- function(df, sf){
-  sf <- sf %>%
-    select(ROUTE_NAME, geometry)
-  df <- left_join(df, sf, by = c("Route_Name" = "ROUTE_NAME")) %>%
-    st_as_sf(wkt=geometry)
-  df
-}

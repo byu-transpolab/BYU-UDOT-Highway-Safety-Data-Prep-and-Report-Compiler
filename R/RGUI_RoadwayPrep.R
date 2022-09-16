73dc145a4a325b7775c42de8fe2061c63b706a50
Tomas Barriga <barriga.tommy@gmail.com>
2022-09-16 12:01:50 -0600

    Change all NAs in Shoulders to zeros

--- R/RGUI_RoadwayPrep.R
+++ R/RGUI_RoadwayPrep.R
@@ -259,6 +259,16 @@ subset_cols <- list(RC %>%
   select(contains("MEDIAN_TYP_")) %>% 
   colnames())
 
+# Change all NA shoulders to 0
+RC$Right_Shoulder[is.na(RC$Right_Shoulder)] <- 0
+RC$Right_Shoulder_Max[is.na(RC$Right_Shoulder_Max)] <- 0 
+RC$Right_Shoulder_Min[is.na(RC$Right_Shoulder_Min)] <- 0 
+RC$Right_Shoulder_Avg[is.na(RC$Right_Shoulder_Avg)] <- 0 
+RC$Left_Shoulder[is.na(RC$Left_Shoulder)] <- 0
+RC$Left_Shoulder_Max[is.na(RC$Left_Shoulder_Max)] <- 0 
+RC$Left_Shoulder_Min[is.na(RC$Left_Shoulder_Min)] <- 0
+RC$Left_Shoulder_Avg[is.na(RC$Left_Shoulder_Avg)] <- 0 
+
 # Run the fill_all_missing function to interpolate remaining data
 RC <- fill_all_missing(RC, missing, "ROUTE", subsetting_var, subset_cols)
 
